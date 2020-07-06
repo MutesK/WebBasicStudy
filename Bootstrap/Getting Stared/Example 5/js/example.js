1052fde3306dbf2b36ec5407b78454986d78d5fe
Mute(JunMin Kim) <j0803@naver.com>
2020-07-06 16:07:34 +0900

    Bootstrap 공부 ( html, view classes... )

---
+++ Bootstrap/Getting Stared/Example 5/js/example.js
@@ -0,0 +1,12 @@
+function buttonClickEvent(event)
+{
+    switch(event.target.id)
+    {
+        case 'button1':
+            alert('button1을 누르셨습니다.');
+            break;
+        case 'button2':
+            alert('button2을 누르셨습니다.');
+            break;
+    }
+}
